63ee6696ee577ef30e55f0097d2ac47d7625a920
SusieR-IT371 <susan.reimer96@gmail.com>
2025-02-06 21:02:42 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,3 +3,10 @@ bgMusic.loop = true;
 window.addEventListener("load", () => {
   bgMusic.play().catch(error => console.log("Autoplay prevented:", error));
 });
+document.getElementById("musicToggle").addEventLister("click", () => {
+  if (bgMusic.paused) {
+    bgMusic.play();
+  } else {
+    bgMusic.pause();
+  }
+});

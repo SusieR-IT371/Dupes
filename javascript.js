c4f1c4f23f520cc8c8a6b7123affcae00efe479c
SusieR-IT371 <susan.reimer96@gmail.com>
2025-02-06 20:56:49 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
-<script>
-var audio = new Audio('best-background-music-no-copyright-244084.mp3');
-audio.play();
-</script>
+const bgMusic = new Audio("best-background-music-no-copyright-244084.mp3");
+bgMusic.loop = true;
+window.addEventListener("load", () => {
+  bgMusic.play().catch(error => console.log("Autoplay prevented:", error));
+});
